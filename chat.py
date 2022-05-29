3abe9472ac7bf5b7106b40933687fd8f3e306388
malgorzatabaranska <mb.malgorzatabaranska@gmail.com>
2022-05-29 18:56:33 +0200

    Second commit with Nick update in messages

--- chat.py
+++ chat.py
@@ -2,16 +2,8 @@
 # Aplication send masseges betwen two chatbox window and using local Server
 # Every messages are archive in Data Base (PostgreSQL)
 
+from email import message
 from socket import *
-
-
-class User(object):
-    """Class for User"""
-    # Gets User Name (Nick)
-    
-
-    def __init__(self):
-        self.nick = input("Wprowadź swój nick: ")
         
 
 class Client(object): 
@@ -19,8 +11,9 @@ class Client(object):
 
     SERVER_ADRESS = "127.0.0.1"
     PORT_SERVER = 10000
+    nick = input("Wprowadź swój nick: ")
 
-    def __init__(self, user):
+    def __init__(self):
         self.connection()
 
 
@@ -29,33 +22,33 @@ class Client(object):
         self.c = socket(AF_INET, SOCK_STREAM)
         #connect to server 
         self.c.connect((self.SERVER_ADRESS, self.PORT_SERVER))
-        welcome = self.c.recv(2048).decode('utf-8')
+        self.c.send(self.nick.encode('ascii'))
+        welcome = self.c.recv(2048).decode('ascii')
         print(welcome)
         self.input_message()
+
     
     def input_message(self):
         """gets form user messages"""
-        self.user_message = input("Wprowadż wiadomość: ")
-        
-        self.send_massage(self.user_message)
+        nick = self.nick
+        self.user_message = input('')
+        self.send_massage(nick, self.user_message)
 
 
     def from_user_reciving(self):
         # Recive message from User        
         while True: 
-            
-            message = self.c.recv(2048)
-            print(message)
+            message_form_user = self.c.recv(2048)
+            print(message_form_user)
             self.input_message()
-
-
-    def send_massage(self, message):
-        while True: 
-            user_message = message
-            print("\x1b[1;36;40m{}\x1b[0m {}".format(" You " , user_message))
-            self.c.send((user_message).encode('utf-8'))
-            self.from_user_reciving()
             
+
+    def send_massage(self, nick, mgs):
+        while True: 
+            message = (nick  + ":" + mgs)
+            print("\x1b[1;36;40m{}\x1b[0m {}".format("You", self.user_message))
+            self.c.send(message.encode('ascii'))
+            self.from_user_reciving()         
             
 
 #class Window(object):
@@ -63,31 +56,12 @@ class Client(object):
     # Reciving from Server
         # messages
         # Nick
-    # Printing date messages in Users windows  
-    #def __init__(self, sv_message):
-     #   self.message = sv_message.message_to_send
-      #  self.text_window(self.message)
 
-
-    
-    #def text_window(self, message):
-     #   update_message = message
-      #  print(update_message)
-        
-
-
-class Aplication(object):
-    "Apliction"
-
-    def __init__(self, user1):
-        client_user1 = Client(user1)
-       
-       
 
 def main():
-    app_user = User()
+    client= Client()
    # box = Window(client_user)
-    app = Aplication(app_user)
+  
 
 main()
 
--- server.py
+++ server.py
@@ -1,88 +1,124 @@
+
+
+from datetime import datetime
 from socket import *
 from threading import Thread
 import threading
+import psycopg2
 
 
-#"""Server is  using for communicate betwen two Users"""
+class Server(object):
 
-# Sending and reciving:
+    #"""Server is  using for communicate betwen two Users"""
+
+    # Sending and reciving:
     # messages 
         # User - Server - User 
         # User - Server - Data Base - Server(future)
 
-IP_SERVER = ""
-PORT_SERVER = 10000
-
-  
-soc = socket(AF_INET, SOCK_STREAM)
-soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-soc.bind((IP_SERVER, PORT_SERVER))
-soc.listen(10)
-list_of_clients = []
-thread = []
+    IP_SERVER = ""
+    PORT_SERVER = 10000 
 
 
-def client(c, addr):
-    c.send(("Welcome to chatroom!").encode('utf-8'))
-    while True:
-        try:
-            message = c.recv(2048)
-            if message:
-                message_to_send = message
-                broadcast(message_to_send, c)
-            else: 
-                remove(c)
-        except: 
-            continue
+    def __init__(self):
+        self.list_of_clients = []
+        self.thread = []
+        self.nick = []
+        self.connection()
+
+
+    def connection(self):
+        self.soc = socket(AF_INET, SOCK_STREAM)
+        self.soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.soc.bind((self.IP_SERVER, self.PORT_SERVER))
+        self.soc.listen(10)
+        self.running()
+        
+
+    def client(self, c, a):
+        c.send(("Welcome to chatroom!").encode('ascii'))
+        while True:
+            try:
+                message = c.recv(2048)
+                if message:
+                    message_to_send = message
+                    self.broadcast(message_to_send, c)
+                else: 
+                    self.remove(c)
+            except: 
+                continue
         
 
 
-def broadcast(message, connection):
-    """Using the below function, we broadcast the message to all
-    clients who's object is not the same as the one sending
-    the message """
-    for clients in list_of_clients: 
-        if clients != connection:
-            try:
-                clients.send(message)
-            except: 
-                clients.close()
-                remove(clients) 
+    def broadcast(self, message, connection):
+        """Using the below function, we broadcast the message to all
+        clients who's object is not the same as the one sending
+        the message """
+        for clients in self.list_of_clients: 
+            if clients != connection:
+                try:
+                    clients.send(message)
+                   # Database.send_data(self.user, message)
+                except: 
+                    clients.close()
+                    self.remove(clients) 
 
     
-def remove(connection):
-    """The following function simply removes the object
-    from the list that was created"""
-    if connection in list_of_clients:
-        list_of_clients.remove(connection)
+    def remove(self, connection):
+        """The following function simply removes the object
+        from the list that was created"""
+        if connection in self.list_of_clients:
+            self.list_of_clients.remove(connection)
+            index = self.list_of_clients(connection)
+            nickname = self.nick[index]
+            self.nick.remove(nickname)
 
 
-while True:
+    def running(self):
+        while True:
  
-    """Accepts a connection request and stores two parameters,
-    conn which is a socket object for that user, and addr
-    which contains the IP address of the client that just
-    connected"""
-    conn, addr = soc.accept()
-
-    thread_client = threading.Thread(target = client, args=(conn, addr))
-    thread.append(thread_client)
-    thread_client.start()
-    list_of_clients.append(conn)
+            """Accepts a connection request and stores two parameters,
+            conn which is a socket object for that user, and addr
+            which contains the IP address of the client that just
+            connected"""
+            conn, addr = self.soc.accept()
+            self.user = conn.recv(2048)
+            self.nick.append(self.user)
+            thread_client = threading.Thread(target=self.client, args=(conn, addr))
+            self.thread.append(thread_client)
+            thread_client.start()
+            self.list_of_clients.append(conn)
   
 
-for t in thread_client:
-    t.join()
+        for t in thread_client:
+            t.join()
 
-conn.close()
-server.close()
+        self.conn.close()
+        self.soc.close()
 
-        
 
-    #def server_sending_db(self, message):
-
-        #wysyłka wiadomości do DB- jeszcze nie wiem jak
-
-        #wysylka wiadomości do okna
-        #self.message_to_send = message.server_message
+class Database(object):
+    #Database in POSTGRESQL is responsible for sending and archive
+    #data (Nick, massage and date)
    
+
+    def connection(self):
+        self.conn = psycopg2.connect("dbname='pyapp' user='gosia' password='123'")
+
+    def send_data(self, user, message):
+        nick = user
+        data = message
+        cur =  self.conn.cursor()
+
+        try: 
+            cur.execute("INSERT INTO achive(nick,message,date) VALUES(%s, %s)", (nick, data, datetime))
+        except: 
+            print("Failed to insert data in achive table") 
+    
+   
+def main():
+
+    sv = Server() 
+    db = Database(sv)
+
+main()
